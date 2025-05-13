ef4d92cc9777b86e4312c6b497bb639c91c5fc13
jvam90 <jvam90@gmail.com>
2025-05-12 21:43:48 -0300

    Add db file, requirements and more validation to endpoints

--- flask_tutorial/app.py
+++ flask_tutorial/app.py
@@ -1,62 +1,63 @@
+import uuid
 from flask import Flask, request
+from db import items, stores
+from flask_smorest import abort
 
 app = Flask(__name__)
 
-stores = [
-    {
-        "name": "Store 1",
-        "active": True,
-        "items": [
-            {
-                "name": "Chair",
-                "price": 100.00
-            },
-            {
-                "name": "Desk",
-                "price": 200.00
-            },
-        ]
-    },
-    {
-        "name": "Store 2",
-        "active": True,
-        "items": [
-            {
-                "name": "Bed",
-                "price": 300.00
-            },
-            {
-                "name": "Wardrobe",
-                "price": 500.00
-            },
-        ]
-    }
-]
-
 @app.get("/stores") #decorador para dizer que o método vai ser invocado ao receber um GET em /stores
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
+
+@app.get("/stores/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+    
+@app.get("/items")
+def get_all_items():
+    return {"items": list(items.values())}
+
+@app.get("/items/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found")
 
 @app.post("/stores")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "active": True, "items": request_data["items"]}
+    store_data = request.get_json()
+
+    if "name" not in store_data:
+        abort(400, message="Ensure that the 'name' is included in the payload.")
+    
+    for store in stores.values():
+        if store["name"] == store_data["name"]:
+            abort(400, message="Cannot add the same store more than once.")
+
+    store_id = uuid.uuid4().hex
+    new_store = {**store_data, "id": store_id}
     stores.append(new_store)
     return new_store, 201
 
-@app.post("/stores/<string:name>/item")
-def create_item(name):
-    item = request.get_json()
-    for store in stores:
-        if store["name"] == name:
-            new_item = {"name": item["name"], "price": item["price"]}
-            store["items"].append(new_item)
-            return item,201
-    return {"message": "Store not found"}, 404
+# agora o id da loja é passado no payload
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
 
-@app.get("/stores/<string:name>/items")
-def get_store_items(name):
-    for store in stores:
-        if store["name"] == name:
-            return store["items"], 200
-    return {"message": "Store not found"}, 404
+    if "price" not in item_data or "store_id" not in item_data or "name" not in item_data:
+        abort(400, message="Ensure that 'price', 'store_id' and 'name' are included in the payload.")
+
+    if item_data["store_id"] not in stores:
+        abort(404, message="Store not found")
+
+    for item in items.values:
+        if (item["name"] == item_data["name"] and item["store_id"] == item_data["store_id"]):
+            abort(400, message="Cannot add the same item more than once.")
+
+    item_id = uuid.uuid4().hex
+    new_item = {**item_data, "id": item_id}
+    items[item_id] = new_item
